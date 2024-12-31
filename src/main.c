705e87aae9ded57d2c790c8674cc8cdccbf4498a
Ahmed Chaudhry <achaud16@u.rochester.edu>
2024-12-31 01:45:38 -0500

    shell input loop set up

---
+++ src/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void shell_loop(void){
+    char *line = malloc(1024); //line from the user
+    char **args; // arguments to the line
+    int status; // Status of the loop(0 or 1)
+
+    if (line == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+    }
+
+
+    int i = 0;
+
+    while (i < 5){
+        printf("> ");
+        
+        if (scanf(" %1023[^\n]", line) == 1){
+            printf("%s \n", line);
+        }
+        else {
+            fprintf(stderr, "printing failed \n");
+        }
+        
+        getchar();
+        i++;
+    }
+
+
+    free(line);
+}
+
+
+int main(int argc, char **argv){
+    shell_loop();
+
+
+    return 0;
+}
